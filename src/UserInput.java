2c58384198c6bde56998c3b642e209bbbc41e2cc
David <david@fagan-1.com>
2019-05-13 12:02:54 +0100

    restricted incorrect input

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,17 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 
 public class UserInput {
-    String v; //user input
+    String v; //(value) = user input
     String[] stdGestures = {"Rock", "Paper", "Scissors", "Lizard", "Spock"}; //accepted values
+    int count;
 
-    public String askUser(){
-        v = JOptionPane.showInputDialog("Please type your gesture, either;\nRock, Paper, Scissors, Lizard or Spock");
-        validateInput();
-        return v;
+    // Method for getting the user input.
+    public String askUser() {
+        try {
+            for (int i = 0; i <= 3; i++) {
+                v = JOptionPane.showInputDialog("Please type your gesture, either;\nRock, Paper, Scissors, Lizard or Spock");
+                count++;
+                if (v == null) //cancel option is selected
+                {
+                    System.exit(0);
+                }
+                else if (count > 3) // if user is messing about
+                {
+                    JOptionPane.showMessageDialog(null,"User input incorrectly too many times, program will now exit!\n\nProgram only accepts the aforementioned gestures.","Input Error",JOptionPane.ERROR_MESSAGE);
+                    System.exit(0);
+                }
+                validateInput();
+            }
+            return v;
+        } catch (Exception e) {
+            System.out.println("User input error" + e);
+            return "rock"; //if there is an error display it in the console and give default response
+        }
     }
 
-    //Method for getting the user input. Only accepts one of the standard gestures (stdGestures).
+    //Validates user input against the standard gestures (stdGestures). If ok returns the value, if not asks for input again.
     public String validateInput() {
         try {
             boolean validInput = Arrays.stream(stdGestures).anyMatch(v::equalsIgnoreCase); // checks user input (v) against the array of values (stdGestures).
@@ -26,7 +46,8 @@ public class UserInput {
                 askUser();
             }
         } catch (Exception e) {
-            return "UserInput error" + e.getLocalizedMessage();
+            System.out.println("User input validation error" + e);
+            return "rock"; //if there is an error display it in the console and give default response
         }
         return v;
     }
